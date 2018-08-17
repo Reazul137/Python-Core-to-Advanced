671a090acf4a29eda62f05207613fb30f3f9139a
Md Reazul Islam <reazulislam137@gmail.com>
2018-08-17 20:14:28 +0600

    Add files via upload

---
+++ First Program/First Program.py
@@ -0,0 +1,14 @@
+'''
+ this is multi
+ line comment
+'''
+#This is my first python program
+print('python is easy!')
+
+print ("Hello World!")
+print ("Hello Again")
+print ("I like typing this.")
+print ("This is fun.")
+print ('Yay! Printing.')
+print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this.')
